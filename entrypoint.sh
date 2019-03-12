73adf2c9b0254bcea8d75a4e8bf89e422ba4c14f
ethicalmohit <mail@imohit.me>
2019-03-12 18:09:12 +0530

    Removed extra blocks of code. Adding local.json value replacement to use environmental variable instead.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,14 @@
 #!/bin/sh
 
-# if [ -z "${LOCAL_JSON}" ]; then
-#   [ -z "${MYSQL_HOST}" ] && export MYSQL_HOST=""
-#   [ -z "${MYSQL_USER}" ] && export MYSQL_USER=""
-#   [ -z "${MYSQL_PASS}" ] && export MYSQL_PASS=""
-  
+# Setting up configuration for phabricator db connection and mail  
 sed -e "s/PHABRICATOR_HOST/$PHABRICATOR_HOST/g" \
   -e "s/MYSQL_HOST/"$MYSQL_HOST"/g" \
   -e "s/MYSQL_USER/"$MYSQL_USER"/g" \
   -e "s/MYSQL_PASS/"$MYSQL_PASS"/g" \
+  -e "s/AWS_SES_ACCESS_KEY/"$AWS_SES_ACCESS_KEY"/g" \
+  -e "s/AWS_SES_SECRET_KEY/"$AWS_SES_SECRET_KEY"/g" \
+  -e "s/AWS_SES_REGION/"$AWS_SES_REGION"/g" \
   -i /opt/phabricator/conf/local/local.json
-
-sed -e "s/AWS_SES_ACCESS_KEY/"$AWS_SES_ACCESS_KEY"/g" \
-    -e "s/AWS_SES_SECRET_KEY/"$AWS_SES_SECRET_KEY"/g" \
-    -e "s/AWS_SES_REGION/"$AWS_SES_REGION"/g" \
-    -i /opt/phabricator/conf/local/mailer.json 
-
-/opt/phabricator/bin/config set --stdin cluster.mailers < /opt/phabricator/conf/local/mailer.json
-
-# else
-#   echo "${LOCAL_JSON}" > /opt/phabricator/conf/local/local.json
-#   echo 
-# fi
 
 if [ "${1}" = "start-server" ]; then
   exec bash -c "/opt/phabricator/bin/storage upgrade --force; /opt/phabricator/bin/phd start; source /etc/apache2/envvars; /usr/sbin/apache2 -DFOREGROUND"
