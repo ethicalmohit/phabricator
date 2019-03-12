e4a36e3003b358c3a27e0edad7ee3170981d67ab
ethicalmohit <mail@imohit.me>
2019-03-12 16:11:55 +0530

    Replacing JSON variables with environment variables at the time of runtime.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,22 @@
 #   [ -z "${MYSQL_USER}" ] && export MYSQL_USER=""
 #   [ -z "${MYSQL_PASS}" ] && export MYSQL_PASS=""
   
-#   # Patching the settings file.
-#   sed -e "s/{{MYSQL_HOST}}/${MYSQL_HOST}/g" \
-#     -e "s/{{MYSQL_USER}}/${MYSQL_USER}/g" \
-#     -e "s/{{MYSQL_PASS}}/${MYSQL_PASS}/g" \
-#     -i /opt/phabricator/conf/local/local.json
+sed -e "s/PHABRICATOR_HOST/$PHABRICATOR_HOST/g" \
+  -e "s/MYSQL_HOST/"$MYSQL_HOST"/g" \
+  -e "s/MYSQL_USER/"$MYSQL_USER"/g" \
+  -e "s/MYSQL_PASS/"$MYSQL_PASS"/g" \
+  -i /opt/phabricator/conf/local/local.json
+
+sed -e "s/AWS_SES_ACCESS_KEY/"$AWS_SES_ACCESS_KEY"/g" \
+    -e "s/AWS_SES_SECRET_KEY/"$AWS_SES_SECRET_KEY"/g" \
+    -e "s/AWS_SES_REGION/"$AWS_SES_REGION"/g" \
+    -i /opt/phabricator/conf/local/mailer.json 
+
+/opt/phabricator/bin/config set --stdin cluster.mailers < /opt/phabricator/conf/local/mailer.json
+
 # else
 #   echo "${LOCAL_JSON}" > /opt/phabricator/conf/local/local.json
+#   echo 
 # fi
 
 if [ "${1}" = "start-server" ]; then
